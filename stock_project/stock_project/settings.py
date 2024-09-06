abe6c2ed7f99f56521d75b6e611ca35921f02042
ChanduAI9 <saichandu9.dev@gmail.com>
2024-09-06 23:41:26 +0530

    Update settings.py

--- stock_project/stock_project/settings.py
+++ stock_project/stock_project/settings.py
@@ -75,12 +75,24 @@ WSGI_APPLICATION = "stock_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',  
+        'USER': 'myuser',      
+        'PASSWORD': 'tiptop123', 
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
